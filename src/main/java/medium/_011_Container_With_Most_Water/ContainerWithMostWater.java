4dffcdcef4407de59dfb41fd0e8db65ab0552c73
Gavin <guowenlong20000@sina.com>
2018-10-20 00:10:11 +0800

    Commit 011 Container With MostWater

--- src/main/java/medium/_011_Container_With_Most_Water/ContainerWithMostWater.java
+++ src/main/java/medium/_011_Container_With_Most_Water/ContainerWithMostWater.java
@@ -20,7 +20,14 @@ public class ContainerWithMostWater {
     public int maxArea(int[] height) {
         return 0;
     }
-
+    /**
+     * 最开始考虑的是暴力破解 但是时间复杂度肯定不能通过...
+     * 所以就没有继续考虑下去
+     *
+     * 从最左面的板和最右面的板子 逐渐缩小到中间 遍历对比
+     * 两个板子 哪个最小 就让他向中心靠近 直到缩短到重合前为止
+     * 返回最大的矩形面积就是最大值
+     */
     public int myMaxArea(int[] height) {
         int max = 0;
         for (int i = 0, j = height.length - 1; i < j; ) {
@@ -35,28 +42,41 @@ public class ContainerWithMostWater {
         return max;
     }
 
+    /**
+     * 我的答案和官方答案差不多
+     */
     public int solutionMaxArea(int[] height) {
-        return 0;
+        int maxarea = 0, l = 0, r = height.length - 1;
+        while (l < r) {
+            maxarea = Math.max(maxarea, Math.min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r])
+                l++;
+            else
+                r--;
+        }
+        return maxarea;
     }
 
+    /**
+     * 国内最快答案
+     */
     public int faststMaxArea(int[] height) {
         int length = height.length;
-        int maxAres = (length-1)*Math.min(height[0],height[length-1]);
+        int maxAres = (length - 1) * Math.min(height[0], height[length - 1]);
         int temp = 0;
         int left = 0;
-        int right = length-1;
-        while(left<right){
-            int temMax = (right-left)*Math.min(height[right],height[left]);
-            if(temMax>maxAres) maxAres = temMax;
-            if(height[left]<height[right]){
+        int right = length - 1;
+        while (left < right) {
+            int temMax = (right - left) * Math.min(height[right], height[left]);
+            if (temMax > maxAres) maxAres = temMax;
+            if (height[left] < height[right]) {
                 temp = left;
-                while(height[temp]>=height[left]&&left<right){
+                while (height[temp] >= height[left] && left < right) {
                     left++;
                 }
-            }
-            else{
+            } else {
                 temp = right;
-                while(height[temp]>=height[right]&&left<right){
+                while (height[temp] >= height[right] && left < right) {
                     right--;
                 }
             }
